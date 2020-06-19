56b0f3ec52c790a4414c02b18481d4a39a3015f5
Mike Hummel <mike.hummel@billingsolutions.de>
2020-06-19 13:22:05 +0200

    Update substitute.py

--- substitute.py
+++ substitute.py
@@ -2,20 +2,33 @@
 
 import sys
 import os
+import argparse
+
+__author__ = "Mike Hummel"
+__copyright__ = "Copyright 2020, Mike Hummel"
+__license__ = "Apache 2.0"
+__version__ = "1.0.0"
+__maintainer__ = "Mike Hummel"
+__email__ = "mike@mhus.de"
+__status__ = "Production"
+
+tagStart = "{{"
+tagEnd = "}}"
 
 def substline(line):
+    global tagStart, tagEnd
     out = "";
     while True:
-        pos = line.find("${")
+        pos = line.find(tagStart)
         if pos < 0:
             break
         out = out + line[0:pos];
-        line = line[pos+2:]
-        pos = line.find("}")
+        line = line[pos+len(tagStart):]
+        pos = line.find(tagEnd)
         if pos < 0:
             break
         key = line[:pos]
-        line = line[pos+1:]
+        line = line[pos+len(tagEnd):]
         default = ""
         t = ""
         pos = key.find(":")
@@ -38,16 +51,38 @@ def substline(line):
     out = out + line
     return out
 
-if len(sys.argv) > 1:
-    file = sys.argv[1]
-    with open(file) as f: content = f.read()
-    f = open(file, "w")
+parser = argparse.ArgumentParser(description='Substitute parameter tags in a file')
+parser.add_argument('-start', help='Tag start delimiter')
+parser.add_argument('-end', help='Tag end delimiter')
+parser.add_argument('source', nargs='?', help='File to substitude or stdin / stdout, this will overwrite the file')
+parser.add_argument('target', nargs='?', help='Output file or stdout')
+
+args = parser.parse_args()
+
+if not args.start is None:
+    tagStart = args.start
+
+if not args.end is None:
+    tagEnd = args.end
+
+#print("From " + tagStart + " to " + tagEnd)
+#print("Source:" + args.source)
+#print("Target:" + args.target)
+
+if args.source is None:
+    for line in sys.stdin:
+        out = substline(line)
+        sys.stdout.write(out);
+elif args.target is None:
+    with open(args.source) as f: content = f.read()
+    for line in content.splitlines():
+        out = substline(line)
+        sys.stdout.write(out);
+else:
+    with open(args.source) as f: content = f.read()
+    f = open(args.target, "w")
     for line in content.splitlines():
         out = substline(line)
         f.write(out)
     f.close()
-else:
-    for line in sys.stdin:
-        out = substline(line)
-        sys.stdout.write(out);
 
