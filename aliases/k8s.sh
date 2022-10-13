7ef7b3df7f05d7657407be30f23b7372e3077e40
Mike Hummel <mh@mhus.de>
2022-10-13 10:09:27 +0200

    Update k8s.sh

--- aliases/k8s.sh
+++ aliases/k8s.sh
@@ -5,6 +5,9 @@ alias kg="kubectl get"
 alias kd="kubectl describe"
 alias kwatch="watch kubectl get"
 alias kevents="kubectl get events --watch"
+alias kctx=kubectx
+alias kns=kubens
 alias d=docker
-
+alias klog="kubectl logs --follow"
+kbash() { kubectl exec -it $@ -- bash } 
 export kd="--dry-run=client -oyaml"
